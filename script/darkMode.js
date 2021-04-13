523a620055348a40a4768d0652b67818b5ad334b
Charles Ciampa <ceciampa@gmail.com>
2021-04-13 01:42:57 -0400

    removing old files

--- script/darkMode.js
+++
@@ -1,8 +0,0 @@
-chrome.storage.sync.get({ darkMode: false, graphMode: true }, (items) => {
-  if (items.darkMode) {
-    //Sends Message to background script to add Dark Mode to Managebac
-    chrome.runtime.sendMessage({
-      darkMode: items.darkMode,
-    });
-  }
-});
